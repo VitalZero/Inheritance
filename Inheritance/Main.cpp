2e7f1bfe95d5a3367b3f95dededf92430e7c20e3
vitalzero <vitalzero@gmail.com>
2019-12-26 16:41:10 -0600

    Se agregó funcion virtual specialmove

--- Inheritance/Main.cpp
+++ Inheritance/Main.cpp
@@ -20,6 +20,135 @@ private:
 	std::mt19937 rng = std::mt19937( std::random_device{}() );
 };
 
+class MemeFighter
+{
+public:
+	std::string GetName() const
+	{
+		return name;
+	}
+	bool IsAlive() const
+	{
+		return hp > 0;
+	}
+	int GetInitiative()
+	{
+		return speed + Roll(2);
+	}
+	void Punch(MemeFighter& target)
+	{
+		if (IsAlive() && target.IsAlive())
+		{
+			std::cout << name << " punches " << target.name << "!.\n";
+			ApplyDamageTo(target, power + Roll(2) );
+		}
+	}
+	void Tick()
+	{
+		if (IsAlive())
+		{
+			int recover = Roll(1);
+			std::cout << "1 turn has elapsed and " << name << " gains " << recover << "hp.\n";
+			hp += recover;
+		}
+	}
+	virtual void SpecialMove(MemeFighter& target)
+	{
+		std::cout << "You will never get this!\n";
+	}
+protected:
+	MemeFighter(const std::string& name, int hp, int speed, int power)
+		:
+		name(name),
+		hp(hp),
+		speed(speed),
+		power(power)
+	{
+		std::cout << GetName() << " enters the ring.\n";
+	}
+	void ApplyDamageTo(MemeFighter& target, int damage)
+	{
+		target.hp -= damage;
+		std::cout << target.GetName() << " takes " << damage << " damage." << std::endl;
+		if (!target.IsAlive())
+		{
+			std::cout << "As the life leaves " << target.GetName() << "'s body, so does the poop.\n";
+		}
+	}
+	int Roll(int nDice = 1) const
+	{
+		return dice.Roll(nDice);
+	}
+protected:
+	int hp;
+	int speed;
+	int power;
+	std::string name;
+private:
+	mutable Dice dice;
+};
+
+class MemeFrog : public MemeFighter
+{
+public:
+	MemeFrog(const std::string& name)
+		:
+		MemeFighter(name, 69, 7, 14)
+	{}
+	void SpecialMove(MemeFighter& target) override
+	{
+		if (IsAlive() && target.IsAlive())
+		{
+			if (Roll() > 4)
+			{
+				std::cout << name << " attacks " << target.GetName() << " with a rainbow beam!.\n";
+				ApplyDamageTo(target, 20 + Roll(3));
+			}
+			else
+			{
+				std::cout << name << " falls off his unicycle.\n"; 
+			}
+		}
+	}
+	void Tick()
+	{
+		if (IsAlive())
+		{
+			std::cout << name << " is AIDS hurt.\n";
+			ApplyDamageTo(*this, Roll());
+			MemeFighter::Tick();
+		}
+	}
+private:
+};
+
+class MemeStoner : public MemeFighter
+{
+public:
+	MemeStoner(const std::string& name)
+		:
+		MemeFighter(name, 80, 4, 10)
+	{}
+	void SpecialMove(MemeFighter& target) override
+	{
+		if (IsAlive() )
+		{
+			if (Roll() > 3)
+			{
+				std::cout << name << " smokes the danky sticky icky, becoming Super " << name << "!.\n";
+				name = "Super " + name;
+				speed += 3;
+				power = (power * 69) / 42;
+				hp += 10;
+			}
+			else
+			{
+				std::cout << name << " spaces out.\n";
+			}
+		}
+	}
+};
+
 void Engage( MemeFighter& f1,MemeFighter& f2 )
 {
 	// pointers for sorting purposes
@@ -35,6 +164,21 @@ void Engage( MemeFighter& f1,MemeFighter& f2 )
 	p2->Punch( *p1 );
 }
 
+void DoSpecials(MemeFighter& f1, MemeFighter& f2)
+{
+	// pointers for sorting purposes
+	auto* p1 = &f1;
+	auto* p2 = &f2;
+	// determine attack order
+	if (p1->GetInitiative() < p2->GetInitiative())
+	{
+		std::swap(p1, p2);
+	}
+	// execute attacks
+	p1->SpecialMove(*p2);
+	p2->SpecialMove(*p1);
+}
+
 int main()
 {
 	MemeFrog f1( "Dat Boi" );
@@ -45,8 +189,7 @@ int main()
 		// trade blows
 		Engage( f1,f2 );
 		// special moves
-		f2.SpecialMove();
-		f1.SpecialMove( f2 );
+		DoSpecials(f1, f2);
 		// end of turn maintainence
 		f1.Tick();
 		f2.Tick();
